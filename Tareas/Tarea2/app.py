b269e9ec00e2d3065275e2d93647863c3bbf49b2
AlMartinez01 <almartinez@ug.uchile.cl>
2023-05-01 13:23:00 -0400

    Se comenzo la edicion de 'agregar pedidos' la parte visual, aun no se configura lo demas

---
+++ Tareas/Tarea2/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+UPLOAD_FOLDER = 'static/svg'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("/menus/initio.html")
+
+
+@app.route('/agregar_pedido', methods=["GET", "POST"])
+def form_add_order():
+    if request.method == "GET":
+        return render_template("/forms/forms_add.html")
+
+@app.route('/agregar_donacion', methods=["GET", "POST"])
+def form_add_don():
+    if request.method == "GET":
+        return render_template("/forms/forms_donation.html")
+
+
+@app.route('/info_donaciones', methods=["GET"])
+def inf_donaciones():
+    if request.method == "GET":
+        return render_template('/menus/info_donation.html', pag = 1)
+
+@app.route('/info_pedidos', methods=["GET"])
+def inf_pedidos():
+    if request.method == "GET":
+        return render_template('/menus/info_order.html', pag = 1)
+
+if __name__ == "__main__":
+    app.run(port=5000)
---
+++ Tareas/Tarea2/static/JavaScripts/agregar-donacion.js
@@ -0,0 +1,376 @@
+const RegionesYcomunas = [
+	{
+		"NombreRegion": "Arica y Parinacota",
+		"comunas": ["Arica", "Camarones", "Putre", "General Lagos"]
+	},
+	{
+		"NombreRegion": "Tarapacá",
+		"comunas": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]
+	},
+	{
+		"NombreRegion": "Antofagasta",
+		"comunas": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"]
+	},
+	{
+		"NombreRegion": "Atacama",
+		"comunas": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"]
+	},
+	{
+		"NombreRegion": "Coquimbo",
+		"comunas": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"]
+	},
+	{
+		"NombreRegion": "Valparaíso",
+		"comunas": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"]
+	},
+	{
+		"NombreRegion": "Región del Libertador Gral. Bernardo O’Higgins",
+		"comunas": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"]
+	},
+	{
+		"NombreRegion": "Región del Maule",
+		"comunas": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+	},
+	{
+		"NombreRegion": "Región de Ñuble",
+		"comunas": ["Cobquecura", "Coelemu", "Ninhue", "Portezuelo", "Quirihue", "Ránquil", "Treguaco", "Bulnes", "Chillán Viejo", "Chillán", "El Carmen", "Pemuco", "Pinto", "Quillón", "San Ignacio", "Yungay", "Coihueco", "Ñiquén", "San Carlos", "San Fabián", "San Nicolás"]
+	},
+	{
+		"NombreRegion": "Región del Biobío",
+		"comunas": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"]
+	},
+	{
+		"NombreRegion": "Región de la Araucanía",
+		"comunas": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"]
+	},
+	{
+		"NombreRegion": "Región de Los Ríos",
+		"comunas": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"]
+	},
+	{
+		"NombreRegion": "Región de Los Lagos",
+		"comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
+	},
+	{
+		"NombreRegion": "Región Aisén del Gral. Carlos Ibáñez del Campo",
+		"comunas": ["Coihaique", "Lago Verde", "Aisén", "Cisnes", "Guaitecas", "Cochrane", "O’Higgins", "Tortel", "Chile Chico", "Río Ibáñez"]
+	},
+	{
+		"NombreRegion": "Región de Magallanes y de la Antártica Chilena",
+		"comunas": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos (Ex Navarino)", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"]
+	},
+	{
+		"NombreRegion": "Región Metropolitana de Santiago",
+		"comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "Santiago", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+	 }
+];
+//Configuracion inicial
+const reg = document.getElementById('regiones');
+const comunas = document.getElementById('comunas');
+const boxFech = document.getElementById('fecha-disponibilidad');
+const fileInput = document.getElementById('fotos');
+const emailInput = document.getElementById('email_donante');
+const direction = document.getElementById('direction');
+const fechaInput = document.getElementById('fecha');
+const number_phone = document.getElementById("number-phone");
+const idCantida = document.getElementById('cantidad');
+const tipeDonation = document.getElementById('tipe-donation');
+const nameDonate = document.getElementById('name_donante');
+const ventanaEmergente = document.getElementById('ventana-emergente ');
+const description = document.getElementById('descripcion')
+const condicionDeRetiro = document.getElementById('condiciones')
+const button = document.getElementById('buttonValidate')
+const form = document.getElementById('form')
+
+
+let predReg = '<option value="0">Seleccione región</option>';
+let predCom = '<option value="0">Seleccione comuna</option>';
+
+for(i = 0; i < RegionesYcomunas.length; i++){
+	predReg = predReg + '<option value="'+ (i+1) + '">'+ RegionesYcomunas[i].NombreRegion + '</option>';
+};
+
+reg.innerHTML = predReg;
+comunas.innerHTML = predCom;
+
+let validate_reg = false;
+let validate_com = false;
+let validate_calleNum = false;
+let validate_tipe = false;
+let validate_cantidad = false;
+let validate_date_disp = false;
+let validate_file = false;
+let validate_name = false;
+let validate_email = false;
+let validate_number = false;
+
+//Cambio de las comunas segun la region
+function validateReg(){
+	predCom = '<option value="0">Seleccione comuna</option>';
+	let val = reg;
+	if (val.value != 0){	
+		for(i = 0; i < RegionesYcomunas[(val.value-1)].comunas.length; i++){
+			predCom = predCom + '<option value="'+ (i+1) + '">'+ RegionesYcomunas[(val.value-1)].comunas[i] + '</option>';
+		};
+		reg.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_reg = true; //a la vez lo validaremos
+	}
+	else{
+		reg.style.border = "2px solid red";
+		validate_reg = false; //Si lo dejan vacio esto no estara validado
+	}
+	comunas.innerHTML = predCom;
+}
+reg.addEventListener('input', (event) => {validateReg()});
+
+function validateComuna(){
+	const val = comunas;
+	if (val.value != 0){	
+		val.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_com = true; //a la vez lo validaremos
+	}
+	else{
+		val.style.border = "2px solid red";
+		validate_reg = false; //Si lo dejan vacio esto no estara validado
+	}
+}
+comunas.addEventListener('input', (event) => {validateComuna()});
+
+function validateDirection(){
+	const val = direction;
+	if (val.value == ""){
+		val.style.border = "2px solid red";
+		validate_calleNum = false; //Si lo dejan vacio esto no estara validado
+	}
+	else if (val.value.length > 80){
+		alert('Se excedio el largo maximo de parametro')
+		val.style.border = "2px solid red";
+		val.value = '';
+		validate_calleNum = false;
+	}
+	else{
+		val.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_calleNum = true; //a la vez lo validaremos
+	}
+}
+direction.addEventListener('input', (event) => {validateDirection()});
+
+
+function validateTypeDonation(){
+	const val = tipeDonation;
+	if (val.value == '0'){
+		val.style.border = "2px solid red";
+		validate_tipe = false; //Si lo dejan vacio esto no estara validado
+	}
+	else{
+		val.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_tipe = true; //a la vez lo validaremos
+	}
+}
+tipeDonation.addEventListener('input', (event) => {validateTypeDonation()});
+
+function validateCantidad(){
+	const val = idCantida;
+	const isValidate = /^[0-9]+[a-zA-Z]+$/;;
+	if (!isValidate.test(val.value)){
+		alert("inserte cantidad valida (por ejemplo 10L)");
+		val.value = '';
+		val.style.border = "2px solid red";
+		validate_cantidad = false; //Si lo dejan vacio esto no estara validado
+	}
+	else{
+		val.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_cantidad = true; //a la vez lo validaremos
+	}
+}
+idCantida.addEventListener('change', (event) => {validateCantidad()});
+
+//Validacion fecha
+function validaFecha(){
+	const fechaIngresada = fechaInput.value;
+	const regexFecha = /^(\d{4})-(\d{2})-(\d{2})$/;
+	
+	if (!regexFecha.test(fechaIngresada)) {
+		alert('Formato de fecha no valido');
+		fechaInput.value = '';
+		fechaInput.style.border = "2px solid red";
+		validate_date_disp = false;
+		return;
+	}
+	
+	const [_, anio, mes, dia] = regexFecha.exec(fechaIngresada);
+    const fechaSeleccionada = new Date(anio, mes-1, dia);
+	const fecha = new Date();
+    fechaActual = new Date(fecha.getFullYear(), fecha.getMonth() ,fecha.getDate());
+    
+    if (fechaSeleccionada.getFullYear() != anio || fechaSeleccionada.getMonth() + 1 != mes || fechaSeleccionada.getDate() != dia) {
+		alert('La fecha ingresada no es válida');
+		fechaInput.value = '';
+		fechaInput.style.border = "2px solid red";
+		validate_date_disp = false;
+		return;
+	}
+	else if (fechaSeleccionada < fechaActual){
+		alert('La fecha ingresada debe ser mayor o igual a la fecha actual');
+		fechaInput.value = '';
+		fechaInput.style.border = "2px solid red";
+		validate_date_disp = false;
+		return;
+	}
+	fechaInput.style.border = "2px solid rgba(0,0,0,0.2)";
+	validate_date_disp = true;
+};
+fechaInput.addEventListener('change', (event) => {validaFecha()});
+
+function validationFiles(){
+	const selectedFiles = fileInput.files;
+	if (selectedFiles.length < 1) {
+		alert('Debe seleccionar al menos 1 archivo');
+		fileInput.style.border = "2px solid red";
+		fileInput.value = '';
+		validate_file = false;
+		return;
+	}
+	if (selectedFiles.length > 3) {
+		alert('Solo puede seleccionar hasta 3 archivos');
+		fileInput.style.border = "2px solid red";
+		fileInput.value = '';
+		validate_file = false;
+		return;
+	}
+	fileInput.style.border = "2px solid rgba(0,0,0,0.2)";
+	validate_file = true;
+	return;
+}
+fileInput.addEventListener('change', (event) => {validationFiles()});
+
+function validateName(){
+	const val = nameDonate;
+	if (val.value.length < 3){	
+		val.style.border = "2px solid red";
+		validate_name = false; //a la vez lo validaremos
+		alert("Nombre demasiado corto");
+		val.value = '';
+	}
+	else if(val.value.length > 80){
+		val.style.border = "2px solid red";
+		validate_name = false; //a la vez lo validaremos
+		alert("Nombre demasiado largo");
+		val.value = '';
+	}
+	else{
+		val.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_name = true; //Si lo dejan vacio esto no estara validado
+	}
+}
+nameDonate.addEventListener('change', (event) => {validateName()});
+
+function validateEmail(){
+	const regexEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+	const emailIngresado = emailInput.value
+
+	if(!regexEmail.test(emailIngresado)){
+		alert('Formato de email no valido');
+		emailInput.value = '';
+		emailInput.style.border = "2px solid red";
+		validate_email = false;
+	}
+	else if (emailIngresado.length > 80){
+		alert('Email demasiado largo');
+		emailInput.style.border = "2px solid red";
+		validate_email = false;
+	}
+	else {
+		emailInput.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_email = true;
+	}
+}
+emailInput.addEventListener('change', (event) => {validateEmail()});
+
+function validateNumberCont(){
+	const regexTelefono = /^\+569\d+$/;
+	const numeroTelefono = number_phone.value;
+	if (numeroTelefono == ''){
+		number_phone.style.border = "2px solid red";
+		validate_number = false;
+	}
+	else if (!regexTelefono.test(numeroTelefono)) {
+		number_phone.value = '';
+		alert('El número de teléfono ingresado no es válido');
+		number_phone.style.border = "2px solid red";
+		validate_number = false;
+	}
+	else if (numeroTelefono.length > 15){
+		alert('El número de teléfono demasiado largo');
+		number_phone.style.border = "2px solid red";
+		validate_number = false;
+	}
+	else{
+		number_phone.style.border = "2px solid rgba(0,0,0,0.2)";
+		validate_number = true;
+	}
+}
+number_phone.addEventListener('change', (event) => validateNumberCont())
+
+function validateLenght(direc){
+	if (direc.value.length >= 80){
+		direc.value = ''
+		direc.style.border = "2px solid red";
+		alert('Demasiado largo')
+	}
+	else direc.style.border = "2px solid rgba(0,0,0,0.2)";
+}
+description.addEventListener("input",(event) => validateLenght(description))
+condicionDeRetiro.addEventListener("input",(event) => validateLenght(condicionDeRetiro))
+
+button.addEventListener('click',function(evento) {
+	evento.preventDefault();
+	let validate = true;
+
+	if (!validate_reg){
+		validateReg();
+		validate=false;
+	}
+	if (!validate_com ){
+		validateComuna();
+		 validate=false;
+	}
+	if (!validate_calleNum){
+		validateDirection();
+		validate=false;
+	}
+	if (!validate_tipe){
+		validateTypeDonation();
+		validate=false;
+	}
+	if (!validate_cantidad){
+		validateCantidad();
+		validate=false;
+	}
+	if (!validate_date_disp){
+		validaFecha();
+		validate=false;
+	}
+	if (!validate_file){
+		validationFiles();
+		validate=false;
+	}
+	if (!validate_name){
+		validateName();
+		validate=false;
+	}
+	if (!validate_email){
+		validateEmail();
+		validate=false;
+	}
+	if (!validate_number){
+		validateNumberCont();
+		validate=false;
+	}
+
+
+	if (validate){
+		console.log("se confirmo")
+		ventanaEmergente.style.display = "block"
+	}
+})
